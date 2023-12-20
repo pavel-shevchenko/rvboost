46b540ea7cd2c0b5c6bf1a0f90a3dbe09925a903
Pavel Shevchenko <e-rider@mail.ru>
2023-12-20 22:50:56 +0300

    fix bugs

--- apps/front_main/next.config.js
+++ apps/front_main/next.config.js
@@ -1,6 +1,11 @@
+const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // transpilePackages: ["ui"], // ! For share
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../')
+  },
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true
