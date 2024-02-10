1a937050ec2f5fdd163b7fa1396c04b17431e387
Pavel Shevchenko <e-rider@mail.ru>
2024-02-11 01:41:46 +0300

    Analytics (6)

--- apps/api/src/common/helpers/common.ts
+++ apps/api/src/common/helpers/common.ts
@@ -4,3 +4,13 @@ export const generateShortLinkCode = () =>
   randomStringGenerator().substring(0, 12);
 
 export const getS3qrCodeFullKey = (s3shortKey: string) => `${s3shortKey}.png`;
+
+export function addMonths(date: Date, months: number) {
+  var result = new Date(date),
+    expectedMonth = (((date.getMonth() + months) % 12) + 12) % 12;
+  result.setMonth(result.getMonth() + months);
+  if (result.getMonth() !== expectedMonth) {
+    result.setDate(0);
+  }
+  return result;
+}
--- apps/api/src/location/entity/location.entity.ts
+++ apps/api/src/location/entity/location.entity.ts
@@ -1,17 +1,15 @@
-import {
-  Collection,
-  Entity,
-  Formula,
-  ManyToOne,
-  OneToOne,
-  Property
-} from '@mikro-orm/core';
+import { Entity, Formula, ManyToOne, OneToOne, Property } from '@mikro-orm/core';
 import { EventEnum, ILocation } from 'typing';
 import { PermissionSubject } from 'casl';
 import { BaseEntity } from '../../common/entities/base.entity';
 import { Organization } from '../../organization/entity';
 import { Card } from '../../card/entity';
 import { CrudEntityFilter } from '../../common/permissions';
+import { addMonths } from '../../common/helpers/common';
+
+function getMinusOneMonthSQL() {
+  return addMonths(new Date(), -1).toISOString().slice(0, 19).replace('T', ' ');
+}
 
 @CrudEntityFilter(PermissionSubject.entityLocation)
 @Entity({
@@ -64,4 +62,56 @@ export class Location extends BaseEntity<Location> implements ILocation {
     { lazy: true }
   )
   avgRating?: number;
+
+  @Formula(
+    (alias) =>
+      `(select count(*) from reviews r where r.location_id = ${alias}.id and created_at >= '${getMinusOneMonthSQL()}')`,
+    { lazy: true }
+  )
+  reviewsCountLast30days?: number;
+
+  @Formula(
+    (alias) =>
+      `(select count(*) from events e where e.event_type = '${
+        EventEnum.followExternalLink
+      }' and e.card_id in (select id from cards c where c.location_id = ${alias}.id) and e.created_at >= '${getMinusOneMonthSQL()}')`,
+    { lazy: true }
+  )
+  externalFollowEventsCntLast30days?: number;
+
+  @Formula(
+    (alias) =>
+      `(select count(*) from events e where e.event_type = '${
+        EventEnum.showReviewFormWithRating
+      }' and e.card_id in (select id from cards c where c.location_id = ${alias}.id) and e.created_at >= '${getMinusOneMonthSQL()}')`,
+    { lazy: true }
+  )
+  showRatingFormEventsCntLast30days?: number;
+
+  @Formula(
+    (alias) =>
+      `(select count(*) from events e where e.event_type = '${
+        EventEnum.showReviewFormWithPlatform
+      }' and e.card_id in (select id from cards c where c.location_id = ${alias}.id) and e.created_at >= '${getMinusOneMonthSQL()}')`,
+    { lazy: true }
+  )
+  showPlatformFormEventsCntLast30days?: number;
+
+  @Formula(
+    (alias) =>
+      `(select count(*) from events e where e.event_type = '${
+        EventEnum.showReviewFormWithBad
+      }' and e.card_id in (select id from cards c where c.location_id = ${alias}.id) and e.created_at >= '${getMinusOneMonthSQL()}')`,
+    { lazy: true }
+  )
+  showBadFormEventsCntLast30days?: number;
+
+  @Formula(
+    (alias) =>
+      `(select count(*) from events e where e.event_type = '${
+        EventEnum.submitReviewFormWithBad
+      }' and e.card_id in (select id from cards c where c.location_id = ${alias}.id) and e.created_at >= '${getMinusOneMonthSQL()}')`,
+    { lazy: true }
+  )
+  submitBadFormEventsCntLast30days?: number;
 }
